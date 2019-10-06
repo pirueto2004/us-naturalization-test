504415d21954de235d96cb1a87168e5c840e7f78
Julio <juliobell2014@gmail.com>
2019-10-06 12:00:23 -0400

    First commit to Git

---
+++ js/main.js
@@ -0,0 +1,84 @@
+﻿$(document).ready(function(){
+
+    ///////////////////////////////////////////////
+    ///////            ACCORDION WIDGET       ///////
+    ///////////////////////////////////////////////
+
+   $('#accordion').accordion({
+	  collapsible: true,
+          active: false,
+          heightStyle: 'content',
+          animate:200,
+          
+
+   });
+
+   $('#accordion1').accordion({
+	  collapsible: true,
+          active: false,
+          heightStyle: 'content',
+          animate:200,
+          
+
+   });
+
+   $('#accordion2').accordion({
+	  collapsible: true,
+          active: false,
+          heightStyle: 'content',
+          animate:200,
+          
+
+   });
+
+	
+   $('#accordion3').accordion({
+	  collapsible: true,
+          active: false,
+          heightStyle: 'content',
+          animate:200,
+          
+
+   });
+
+
+   $('#accordion4').accordion({
+	  collapsible: true,
+          active: false,
+          heightStyle: 'content',
+          animate:200,
+          
+
+   });
+
+
+   $('#accordion5').accordion({
+	  collapsible: true,
+          active: false,
+          heightStyle: 'content',
+          animate:200,
+          
+
+   });
+
+   $('#accordion6').accordion({
+	  collapsible: true,
+          active: false,
+          heightStyle: 'content',
+          animate:200,
+          
+
+   });
+
+
+   $('#accordion7').accordion({
+	  collapsible: true,
+          active: false,
+          heightStyle: 'content',
+          animate:200,
+          
+
+   });
+
+    
+});
